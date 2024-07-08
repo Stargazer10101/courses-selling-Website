167f2ef4a6c84952dceac3f78706063b4e92675b
Nitish Itankar <nitishitankar9102@gmail.com>
2024-07-08 14:18:22 +0530

    commit-3

--- 02-medium-course-selling-app-admin-dashboard/src/components/Register.jsx
+++ 02-medium-course-selling-app-admin-dashboard/src/components/Register.jsx
@@ -7,8 +7,10 @@ function Register() {
     return <div>
         <h1>Register to the website</h1>
         <br/>
+        Email:
         <input type={"text"} onChange={e => setEmail(e.target.value)} />
         <br/>
+        Password:
         <input type={"text"} onChange={e => setPassword(e.target.value)} />
         <br/>
         Already a user? <a href="/login">Login</a>
